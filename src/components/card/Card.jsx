4e317b4b4cd0f70caab75429afb667f7fe0218ba
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-19 14:12:50 -0400

    card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import { CardSuspense } from "./suspense";
+
+const Card = (name, status, image) => {
+  return (
+    <Suspense fallback={<CardSuspense />}>
+      <div className="card-container">
+        <img className="rounded-t-lg" src={image} alt={name} />
+        <div className="p-5">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-subtitle">{status}</p>
+        </div>
+      </div>
+    </Suspense>
+  );
+};
+
+export default Card;
