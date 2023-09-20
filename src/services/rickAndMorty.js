3dac939bf25f0161857b91b1d8eba281d91de3f8
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-20 13:08:35 -0400

    rename baseUrl

--- src/services/rickAndMorty.js
+++ src/services/rickAndMorty.js
@@ -1,4 +1,4 @@
-export const rickAndMorty = "https://rickandmortyapi.com/api/character";
+export const rickAndMortyUrl = "https://rickandmortyapi.com/api/character";
 
 export const getCharacters = async (url) => {
   return await fetch(url).then((res) => res.json());
