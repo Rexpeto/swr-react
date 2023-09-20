a3d29e7f5e27f4f631af14259d83d7bd1a140faf
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-20 13:01:22 -0400

    use routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import { NotUsingSWR } from "@/components";
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Layout } from "@/layout";
+import { Route } from "react-router-dom";
+import { publicRoutes } from "@/models";
+import { NotUsingSWR, UsingSWR } from "@/pages";
 
 const App = () => {
-  return <NotUsingSWR />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path={publicRoutes.NOTUSINGSWR} element={<NotUsingSWR />} />
+          <Route path={publicRoutes.USINGSWR} element={<UsingSWR />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
