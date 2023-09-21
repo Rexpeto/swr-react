3dd601446fbd3e2cdf75582a4084ea790d802c05
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-21 11:11:29 -0400

    swr

--- src/pages/usingSWR/UsingSWR.jsx
+++ src/pages/usingSWR/UsingSWR.jsx
@@ -1,5 +1,28 @@
+import useSWR from "swr";
+import { getCharacters, rickAndMortyUrl } from "@/services";
+import { Card } from "@/components";
+
 const UsingSWR = () => {
-  return <div>UsingSWR</div>;
+  const { data } = useSWR(rickAndMortyUrl, getCharacters, {
+    refreshInterval: 10000,
+    suspense: true,
+  });
+
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-2xl pb-10">Using SWR</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {data?.results.map((charater) => (
+          <Card
+            key={charater.id}
+            name={charater.name}
+            status={charater.status}
+            image={charater.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default UsingSWR;
